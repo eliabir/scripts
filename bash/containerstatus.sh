5e4d599d0b08378a996c5584cd49cf296b10ccc9
Eliad <45791074+eliabir@users.noreply.github.com>
2022-05-10 01:50:02 +0200

    Update containerstatus.sh
    colors

--- bash/containerstatus.sh
+++ bash/containerstatus.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
+# Colors
+BLUE="\e[34m"
+YELLOW="\e[1;33m"
+ENDCOL="\e[0m"
+
 CONTAINER_STATUS="docker compose ps | tail -n 1 | cut -d '(' -f 2 | cut -d ')' -f 1"
 
 PRINT_ONCE=0
 
 CURRENT_STATUS=$(eval "$CONTAINER_STATUS")
 
-echo "Container starting..."
-echo "Status: $CURRENT_STATUS"
+echo "${BLUE}Container starting...${ENDCOL}"
+echo "${BLUE}Status:${ENDCOL} ${YELLOW}$CURRENT_STATUS${ENDCOL}"
 
 while true
 do
     if [[ $(eval "$CONTAINER_STATUS") != $CURRENT_STATUS ]]
     then
         CURRENT_STATUS=$(eval "$CONTAINER_STATUS")
-        echo "Status: $CURRENT_STATUS"
+        echo -e "${BLUE}Status:${ENDCOL} ${YELLOW}$CURRENT_STATUS${ENDCOL}"
     fi
 
     if [[ $(eval "$CONTAINER_STATUS") == "healthy" ]]
     then
-        echo "Container up!"
+        echo "${YELLOW}Container up!${ENDCOL}"
         break
     fi
 done
